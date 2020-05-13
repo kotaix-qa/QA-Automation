0bc1e2f9840f72bc23ec8d480111e9c7f31dbda5
Space Cat <jhauyong.uy@gmail.com>
2020-05-13 19:07:32 -0300

    Update tottusRecetasPage.ts

--- Parte3/Pages/tottusRecetasPage.ts
+++ Parte3/Pages/tottusRecetasPage.ts
@@ -3,5 +3,10 @@ import {Selector,t} from 'testcafe';
 
 export class tottusRecetasPage{
 
+    private readonly lowDifficulty = Selector("button.btnfilter:nth-child(2)");
+    private readonly midDifficulty = Selector("button.btnfilter:nth-child(3)");
+    private readonly highDifficulty = Selector("button.btnfilter:nth-child(4)");
+    private readonly coronaDeRollitosCanela = Selector("div.row:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > h3:nth-child(1)");
 
-};
+}; 
+
