bf62c45fb0060d6f517179ef7f73bb15243dd01d
Space Cat <jhauyong.uy@gmail.com>
2020-05-12 22:15:19 -0300

    Update googlePage.ts

--- Parte3/Pages/googlePage.ts
+++ Parte3/Pages/googlePage.ts
@@ -1,11 +1,17 @@
-import {Selector} from 'testcafe';
+import  {Selector} from 'testcafe';
+import {t} from 'testcafe'
 
 export class GooglePage {
     private readonly searchBar = Selector(".gLFyf");
     private readonly searchBttn = Selector(".FPdoLc > center:nth-child(1) > input:nth-child(1)").withText("Buscar con Google");
     private readonly resultsTotal = Selector("#rso");
+    private readonly resultStoreLocations = Selector("div.rc:nth-child(3) > div:nth-child(1) > a:nth-child(1) > div:nth-child(3) > cite:nth-child(1)");
 
+
+
+    async  setSearchRequest(t, string:setSearch){
+        await typetex
+            
+
+    };
 }
-
-
-
