fbf0d8f64e63cd01bd8f214b9007c20fdca3cf6a
Space Cat <jhauyong.uy@gmail.com>
2020-05-13 15:21:01 -0300

    Page Model Update, new Page added

--- Parte3/Pages/TottusLocationPage.ts
+++ Parte3/Pages/TottusLocationPage.ts
@@ -1,8 +1,9 @@
 import {Selector,t} from 'testcafe';
 
 
-
 export class TottusPage {
-
+    private readonly storesFound = Selector(".title-category-section > h3:nth-child(1)");
+    private readonly recetasYmas = Selector("#new_tottus_nav_cliente_recetas > a:nth-child(1) > img:nth-child(1)");
+    
 
 }
--- Parte3/Test/Tests.ts
+++ Parte3/Test/Tests.ts
@@ -5,8 +5,22 @@ import { t } from 'testcafe';
 const GooglaCLWeb = "https://www.google.com";
 
 fixture("Parte3 - Prueba Tecnica Falabella").page(GooglaCLWeb);
-test("Google Search: Sucursales Tottus", async t=>{
-let startBrowser = new GooglePage
-await startBrowser.setSearchRequest("Tottus Sucursales");
+    test("Google Search: Sucursales Tottus", async t=>{
+        let Browser = new GooglePage
+        await Browser.setSearchRequest("Tottus Sucursales");
 
-});
+    test("Select TottusLocations at Results", async t =>{
+        let Browser = new GooglePage
+        Browser.selectTottusLocationPage
+        
+
+         
+        })
+
+
+    
+  
+
+    });
+
+
